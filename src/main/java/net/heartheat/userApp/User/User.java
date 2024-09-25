677f186dae31c0ccbe007903181d8fec4bbc0fb7
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-26 01:42:24 +0500

    added lombok concept

--- src/main/java/net/heartheat/userApp/User/User.java
+++ src/main/java/net/heartheat/userApp/User/User.java
@@ -1,11 +1,13 @@
 package net.heartheat.userApp.User;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name="user")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,44 +16,4 @@ public class User {
     private String password;
     private LocalDate dob;
     private int age;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
