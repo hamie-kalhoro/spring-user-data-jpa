f11eaae5211792c0c83ebc899972fbbc09342085
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-26 01:55:24 +0500

    added saveUser function for post call

--- src/main/java/net/heartheat/userApp/User/UserService.java
+++ src/main/java/net/heartheat/userApp/User/UserService.java
@@ -1,8 +1,16 @@
 package net.heartheat.userApp.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 
 }
