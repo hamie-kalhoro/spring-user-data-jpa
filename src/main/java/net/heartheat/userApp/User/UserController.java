7371bbe8c7cd79c781a5dc6fdfdc1a39ef1c3e5f
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-26 01:52:43 +0500

    added post call to create a user

--- src/main/java/net/heartheat/userApp/User/UserController.java
+++ src/main/java/net/heartheat/userApp/User/UserController.java
@@ -1,5 +1,22 @@
 package net.heartheat.userApp.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create-user")
+    public boolean createNewUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return true;
+    }
 
 }
--- src/main/java/net/heartheat/userApp/User/UserService.java
+++ src/main/java/net/heartheat/userApp/User/UserService.java
@@ -1,5 +1,8 @@
 package net.heartheat.userApp.User;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
 }
