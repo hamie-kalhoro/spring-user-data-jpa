4f167e0535719fbcaaa417e5916aa91a04cf20c5
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-26 01:35:01 +0500

    initilized Repository interface to communicate and access all the properties present in mySQL database

--- src/main/java/net/heartheat/userApp/User/UserRepository.java
+++ src/main/java/net/heartheat/userApp/User/UserRepository.java
@@ -1,5 +1,9 @@
 package net.heartheat.userApp.User;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
