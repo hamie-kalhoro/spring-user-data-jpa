079d1cdc6d84893210216cef9ac78f228c3f8200
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-26 01:32:59 +0500

    added user class with fields

--- src/main/java/net/heartheat/userApp/User/User.java
+++ src/main/java/net/heartheat/userApp/User/User.java
@@ -1,5 +1,51 @@
 package net.heartheat.userApp.User;
 
-public class User {
+import java.time.LocalDate;
 
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private LocalDate dob;
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
